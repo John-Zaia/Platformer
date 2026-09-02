4679dd93a4e126c61a7b5ec73fae9f72827ca443
John Zaia <john.zaia@outlook.com>
2026-09-01 21:47:16 -0400

    Added player movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,50 @@
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+void setupPlayer(sf::RectangleShape& player)
+{
+	player.setSize(sf::Vector2f(50.f, 100.f));
+	player.setFillColor(sf::Color(100, 250, 50));
+}
+
+void playerMovement(sf::RectangleShape& player, float deltaTime, float speed)
+{
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+	{
+		player.move({ speed * deltaTime, 0.f });
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+	{
+		player.move({ -speed * deltaTime, 0.f });
+	}
+}
 
 int main()
 {
-	sf::Window window(sf::VideoMode({ 800, 600 }), "My Window");
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My Window");
+
+	sf::RectangleShape player;
+	setupPlayer(player);
+
+	sf::Clock clock;
+	float speed = 200.f;
 
 	while (window.isOpen())
 	{
+		float deltaTime = clock.restart().asSeconds();
+
 		while (const std::optional event = window.pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
+		playerMovement(player, deltaTime, speed);
+
+		window.clear(sf::Color::Black);
+		window.draw(player);
+		window.display();
+
 	}
 
 }
